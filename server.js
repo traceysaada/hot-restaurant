2fa6e898aa92d53c07657095afce5a2e40549d18
Tracey Saada <tracey.saada@outlook.com>
2021-01-16 10:56:17 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+// Dependencies
+var express = require(“express”);
+var path = require(“path”);
+
+// set up express app
+var app = express();
+var PORT = 3000;
+
+// sets up the express app to handle data parsing
+
+
+// Reservation (DATA)
+
+
+// Routes to HTML 
+
+
+// basic route that sends the user to the AJAX Page
+
+
+// displays tables & waiting list
+
+
+// displays reservation addition
+
+
+
+// displays a table list
+
+
+// get current tables on the wait list 
+
+
+// Create a reserve post route 
+
+
+// Starts the server to being listening 
+
